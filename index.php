8abe55dd9c8f9c042e86abb1769fd7ced7e2617e
tejalbhilare <tejalbhilare02@gmail.com>
2026-08-23 00:45:19 +0530

    2026-08-23: Created project structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Local Volunteer Matching System</title>
+</head>
+<body>
+
+    <h1>Local Volunteer Matching System</h1>
+
+    <p>Connecting volunteers with local community opportunities.</p>
+
+</body>
+</html>
